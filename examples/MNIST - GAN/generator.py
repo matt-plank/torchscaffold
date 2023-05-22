e9304efd22f9b4c4ea181499ce9ac334658af23b
matt-plank <matthew_plank@outlook.com>
2023-05-22 18:51:47 +0100

    Changed convolutional layer order in MNIST GAN example

--- examples/MNIST - GAN/generator.py
+++ examples/MNIST - GAN/generator.py
@@ -7,21 +7,21 @@ class GeneratorModel(nn.Module):
         super(GeneratorModel, self).__init__()
 
         # Input is 64 random numbers (8x8 random image)
-        self.conv1 = nn.ConvTranspose2d(in_channels=1, out_channels=256, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(256)
+        self.conv1 = nn.ConvTranspose2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
         self.relu1 = nn.ReLU()
         self.upsampling1 = nn.UpsamplingNearest2d(scale_factor=2)
 
-        self.conv2 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.ConvTranspose2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
         self.bn2 = nn.BatchNorm2d(128)
         self.relu2 = nn.ReLU()
         self.upsampling2 = nn.UpsamplingNearest2d(scale_factor=2)
 
-        self.conv3 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, stride=1, padding=0)
-        self.bn3 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=0)
+        self.bn3 = nn.BatchNorm2d(256)
         self.relu3 = nn.ReLU()
 
-        self.conv4 = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=3, stride=1, padding=0)
+        self.conv4 = nn.Conv2d(in_channels=256, out_channels=1, kernel_size=3, stride=1, padding=0)
         self.bn4 = nn.BatchNorm2d(1)
         self.relu4 = nn.ReLU()
 
