bbce0f0867a6ce24e5a8cd0693b0c358f9b935c2
matt-plank <matthew_plank@outlook.com>
2023-05-25 14:46:19 +0100

    Added residual block for conv transpose layers

--- torchscaffold/layers.py
+++ torchscaffold/layers.py
@@ -24,3 +24,28 @@ class ConvResidualBlock(nn.Module):
         x = self.relu2(x)
 
         return x + identity
+
+
+class ConvTransposeResidualBlock(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int):
+        super(ConvTransposeResidualBlock, self).__init__()
+
+        self.conv1 = nn.ConvTranspose2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.ConvTranspose2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu2 = nn.ReLU()
+
+    def forward(self, x):
+        identity = x
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+
+        return x + identity
