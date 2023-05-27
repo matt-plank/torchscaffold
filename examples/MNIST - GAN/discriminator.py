55e6e73e1fd0ac447a0a8c5452fa8d9bcce4c4cf
matt-plank <matthew_plank@outlook.com>
2023-05-27 17:39:08 +0100

    Improved model architectures for generator and discriminator in MNIST GAN example

--- examples/MNIST - GAN/discriminator.py
+++ examples/MNIST - GAN/discriminator.py
@@ -1,40 +1,41 @@
 import torch
 import torch.nn as nn
 
+from torchscaffold.layers import ConvResidualBlock
+
 
 class DiscriminatorModel(nn.Module):
     def __init__(self):
         super(DiscriminatorModel, self).__init__()
 
+        # Residual blocks
+        self.block1 = ConvResidualBlock(in_channels=1, out_channels=32)
+        self.block2 = ConvResidualBlock(in_channels=32, out_channels=32)
+        self.block3 = ConvResidualBlock(in_channels=32, out_channels=32)
+        self.block4 = ConvResidualBlock(in_channels=32, out_channels=32)
+
         # Input shape is 28x28
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(32)
         self.pooling1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.relu1 = nn.ReLU()
 
         # Input shape is 14x14
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(16)
         self.pooling2 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.relu2 = nn.ReLU()
 
         # Input shape is 7x7
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
-        self.bn3 = nn.BatchNorm2d(128)
-        self.pooling3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.relu3 = nn.ReLU()
-
-        # Input shape is 3x3
-        self.conv4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
-        self.bn4 = nn.BatchNorm2d(256)
-        self.pooling4 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.relu4 = nn.ReLU()
-
-        # Input shape is 1x1
         self.flatten = nn.Flatten()
-        self.layer1 = nn.Linear(256, 1)
+        self.layer1 = nn.Linear(7 * 7 * 16, 1)
 
     def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.pooling1(x)
@@ -44,16 +45,6 @@ class DiscriminatorModel(nn.Module):
         x = self.bn2(x)
         x = self.pooling2(x)
         x = self.relu2(x)
-
-        x = self.conv3(x)
-        x = self.bn3(x)
-        x = self.pooling3(x)
-        x = self.relu3(x)
-
-        x = self.conv4(x)
-        x = self.bn4(x)
-        x = self.pooling4(x)
-        x = self.relu4(x)
 
         x = self.flatten(x)
         x = self.layer1(x)
--- examples/MNIST - GAN/generator.py
+++ examples/MNIST - GAN/generator.py
@@ -1,53 +1,56 @@
 import torch
 import torch.nn as nn
 
+from torchscaffold.layers import ConvTransposeResidualBlock
+
 
 class GeneratorModel(nn.Module):
     def __init__(self):
         super(GeneratorModel, self).__init__()
 
-        # Input is 64 random numbers (8x8 random image)
+        # Input to the network is 7x7 random noise
         self.conv1 = nn.ConvTranspose2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(64)
+        self.upsample1 = nn.Upsample(scale_factor=2, mode="nearest")
         self.relu1 = nn.ReLU()
-        self.upsampling1 = nn.UpsamplingNearest2d(scale_factor=2)
 
-        self.conv2 = nn.ConvTranspose2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(128)
+        self.conv2 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.upsample2 = nn.Upsample(scale_factor=2, mode="nearest")
         self.relu2 = nn.ReLU()
-        self.upsampling2 = nn.UpsamplingNearest2d(scale_factor=2)
 
-        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=0)
-        self.bn3 = nn.BatchNorm2d(256)
-        self.relu3 = nn.ReLU()
+        self.block1 = ConvTransposeResidualBlock(in_channels=64, out_channels=64)
+        self.block2 = ConvTransposeResidualBlock(in_channels=64, out_channels=64)
+        self.block3 = ConvTransposeResidualBlock(in_channels=64, out_channels=64)
+        self.block4 = ConvTransposeResidualBlock(in_channels=64, out_channels=64)
 
-        self.conv4 = nn.Conv2d(in_channels=256, out_channels=1, kernel_size=3, stride=1, padding=0)
-        self.bn4 = nn.BatchNorm2d(1)
-        self.relu4 = nn.ReLU()
+        self.conv = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=3, stride=1, padding=1)
+        self.bn = nn.BatchNorm2d(1)
+        self.sigmoid = nn.Sigmoid()
 
     def random(self, batch_size: int, gpu_device):
-        inputs = torch.randn(batch_size, 1, 8, 8).to(gpu_device)
+        inputs = torch.randn(batch_size, 1, 7, 7, device=gpu_device)
 
         return inputs, self.forward(inputs)
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
+        x = self.upsample1(x)
         x = self.relu1(x)
-        x = self.upsampling1(x)
 
         x = self.conv2(x)
         x = self.bn2(x)
+        x = self.upsample2(x)
         x = self.relu2(x)
-        x = self.upsampling2(x)
 
-        x = self.conv3(x)
-        x = self.bn3(x)
-        x = self.relu3(x)
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
 
-        x = self.conv4(x)
-        x = self.bn4(x)
-        x = self.relu4(x)
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.sigmoid(x)
 
-        x = torch.sigmoid(x)
         return x
